d1c9d660b0e837577e7b986cf4d9809dcaad8d81
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-01-20 20:06:53 +0700

    require & exports

---
+++ justTest.js
@@ -0,0 +1,9 @@
+// require
+
+const myMenu = require('./ingredients')
+myMenu.specialMenu()
+console.log(myMenu.menu1)
+
+const Rabbit = require('./rabbit')
+const rabbit1 = new Rabbit('Mimi', 'I dunno')
+console.log(rabbit1)
