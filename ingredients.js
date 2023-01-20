dc5071d6d220b1535765b6c98fbd9e040da3411a
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-01-20 20:06:18 +0700

    require & exports

---
+++ ingredients.js
@@ -0,0 +1,11 @@
+// const menu1 = 'Piranha'
+
+// exports.menu1 = menu1
+exports.menu1 = 'Piranha'
+
+// function specialMenu() {
+//   console.log('Anaconda')
+// }
+
+// exports.specialMenu = specialMenu
+exports.specialMenu = () => console.log('Anaconda')
